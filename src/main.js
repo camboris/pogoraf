afb86802c73b2a70d599ad989cbc46653c3e819b
Mario G. Pozzo <camboris@gmail.com>
2018-04-03 18:48:42 -0300

    Lista de jefes actualizada con CP

--- src/main.js
+++ src/main.js
@@ -1,23 +1,36 @@
+const climas = [
+'🌫', // 0 niebla
+'☃', // 1 nieve
+'☁', // 2 nublado
+'🍃', // 3 viento
+'☀', // 4 sol
+'☂', // 5 luvia
+'⛅' // 6 parcialmente
+]
+
 const jefes = [
     { nivel: 5, nombre: "Huevo nivel 5"},
-    { nivel: 5, nombre: "Lugia"},
+    { nivel: 5, nombre: "Latios", clima: [3], cp_max: "2082", cp_max_clima: "2603"},
+    { nivel: 5, nombre: "Latias", clima: [3], cp_max: "1929", cp_max_clima: "2412"},
     { nivel: 4, nombre: "Huevo nivel 4"},
-    { nivel: 4, nombre: "Absol"},
-    { nivel: 4, nombre: "Agron"},
-    { nivel: 4, nombre: "Golem"},
-    { nivel: 4, nombre: "Tyranitar"},
+    { nivel: 4, nombre: "Absol", clima: [0], cp_max: "1303", cp_max_clima: "1629"},
+    { nivel: 4, nombre: "Aggron", clima: [6, 1], cp_max: "1716", cp_max_clima: "2145"},
+    { nivel: 4, nombre: "Houndoom", clima: [4, 0], cp_max: "1445", cp_max_clima: "1806"},
+    { nivel: 4, nombre: "Tyranitar", clima: [6, 0], cp_max: "2097", cp_max_clima: "2621"},
+    { nivel: 4, nombre: "Walrein", clima: [5, 1], cp_max: "1489", cp_max_clima: "1862"},
     { nivel: 3, nombre: "Huevo nivel 3"},
-    { nivel: 3, nombre: "Gengar"},
-    { nivel: 3, nombre: "Jolteon"},
-    { nivel: 3, nombre: "Jynx"},
-    { nivel: 3, nombre: "Machamp"},
-    { nivel: 3, nombre: "Piloswine"},
+    { nivel: 3, nombre: "Gengar", clima: [2, 0], cp_max: "1496", cp_max_clima: "1870"},
+    { nivel: 3, nombre: "Granbull", clima: [2], cp_max: "1394", cp_max_clima: "1743"},
+    { nivel: 3, nombre: "Jinx", clima: [1, 3], cp_max: "1435", cp_max_clima: "1794"},
+    { nivel: 3, nombre: "Machamp", clima: [2], cp_max: "1650", cp_max_clima: "2063"},
+    { nivel: 3, nombre: "Pinsir", clima: [5], cp_max: "1583", cp_max_clima: "1978"},
+    { nivel: 3, nombre: "Piloswine", clima: [1, 4], cp_max: "1305", cp_max_clima: "1601"},
     { nivel: 2, nombre: "Huevo nivel 2"},
-    { nivel: 2, nombre: "Sneasel (CP 1067 / 🌫☃ 1334)"},
-    { nivel: 2, nombre: "Mawile (CP 848 / ☁☃1060)"},
-    { nivel: 2, nombre: "Misdreavus (CP 1018 / 🌫 1272)"},
-    { nivel: 2, nombre: "Sableye (CP 745 / 🌫932)"},
-    { nivel: 2, nombre: "Exeggutor (CP 1666 / 🍃☀2083)"},
+    { nivel: 2, nombre: "Exeggutor", clima: [3, 4], cp_max: "1666", cp_max_clima: "2083"},
+    { nivel: 2, nombre: "Mawile", clima: [1, 2], cp_max: "848", cp_max_clima: "1060"},
+    { nivel: 2, nombre: "Misdreavus", clima: [0], cp_max: "1018", cp_max_clima: "1272"},
+    { nivel: 2, nombre: "Sableye", clima: [0], cp_max: "745", cp_max_clima: "932"},
+    { nivel: 2, nombre: "Sneasel", clima: [0, 1], cp_max: "1067", cp_max_clima: "1334"},
 ]
 
 //TODO: hacer lista iconos, poner cp aparte, como direccion en los gimansios y concatenar
@@ -80,8 +93,14 @@ var app = new Vue({
             return  g;
         },
         jefeMostrar: function() {
-            const j = this.jefes[this.jefe_elegido].nombre;
-            return  j;
+            const j = this.jefes[this.jefe_elegido];
+            let nj = j.nombre;
+            if(j.clima) {
+                let clima = j.clima.map((c) => climas[c])
+                            .reduce((c, a) => c + a, '');
+                nj = j.nombre + " (" + j.cp_max + " / " + clima + j.cp_max_clima + ")";
+            }
+            return  nj;
         },
         listaCorta: function() {
             if(this.jefes[this.jefe_elegido].nivel <= 3) {
